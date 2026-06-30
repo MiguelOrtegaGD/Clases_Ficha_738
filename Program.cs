58f8eefaf0098a487058806b0612e5c3f6e9c89f
MiguelOrtegaGD <142111227+MiguelOrtegaGD@users.noreply.github.com>
2026-06-30 07:12:31 -0500

    Limpiar Rama

--- Program.cs
+++ Program.cs
@@ -7,24 +7,8 @@ namespace Clases_Ficha_738
     {
         static void Main(string[] args)
         {
-            Guerrero g1 = new Guerrero("Arturito");
-            Mago m1 = new Mago("Merlin");
-            Sacerdote s1 = new Sacerdote("Chucho");
 
-            g1.Atacar(m1);
-            m1.Atacar(g1);
 
-            s1.Curar(m1, 30);
-            g1.Atacar(m1);
-            g1.Atacar(s1);
-            s1.Atacar(g1);
-            g1.Atacar(m1);
-            m1.Atacar(g1);
-            s1.Curar(s1, 50);
-
-            m1.MostrarDatos();
-            g1.MostrarDatos();
-            s1.MostrarDatos();
         }
     }
 }
